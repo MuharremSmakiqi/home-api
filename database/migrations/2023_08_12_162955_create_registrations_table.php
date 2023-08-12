321852cb33234985060ffa72c1d0ff61c7e86a55
Muharrem <muharrem.w2@gmail.com>
2023-08-12 18:48:22 +0200

    Migrations added

---
+++ database/migrations/2023_08_12_162955_create_registrations_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('registrations', function (Blueprint $table) {
+            $table->id();  
+            $table->uuid('uuid')->unique();
+            $table->string('customer_id')->references('id')->on('customers');
+            $table->integer('package_id')->references('id')->on('packages');
+            $table->timestamp('registered_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('registrations');
+    }
+};
